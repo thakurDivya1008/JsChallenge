05c676cdd9403af8efa259dffce05870a81c23fc
Divya <thakurdivya220703@gmail.com>
2024-07-13 19:05:42 +0530

    first commit

---
+++ Data_Variables.js
@@ -0,0 +1,57 @@
+//Activity 1: Variable Declaration
+
+//Task1
+var num = 1;
+console.log(num);
+
+//Task2
+let myName = "Divya Kumari";
+console.log(myName);
+
+//Activity 2: Constant Declaration
+//Task 3
+
+const boolVal = true;
+console.log(boolVal);
+
+//Activity 3: Data Types
+//Task 4
+const name = "Divya Kumari";
+const marks = 95;
+const isPass = true;
+
+const student = {
+   RollNo : "15",
+   isPresent:"true"
+}
+
+const subjects = ["JavaScript","ReactJs"];
+
+console.log(typeof name);
+console.log(typeof marks);
+console.log(typeof isPass);
+console.log(typeof student);
+console.log(typeof subjects);
+
+//Activity 4: Reassigning Variables
+//Task 5
+
+let inValue = 10;
+console.log("Initial Value :",inValue);
+
+let newVal = 20;
+console.log("New Value :",newVal);
+
+//Activity 5: Understanding const
+//Task 6
+
+const Value = 1;
+console.log("Before:", Value);
+
+Value = 2;
+console.log("After :",Value);
+
+
+
+
+
